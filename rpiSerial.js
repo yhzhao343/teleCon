68b58f076ab59246322530e3a86143780217cc28
yhzhao343 <yhzhao343@gmail.com>
2016-02-26 23:47:10 -0500

    fill rpiSerial test

---
+++ rpiSerial.js
@@ -0,0 +1,19 @@
+var SerialPort = require("serialport").SerialPort
+var serialPort = new SerialPort("/dev/ttyAMA0", {
+  baudrate: 9600
+}, false); // this is the openImmediately flag [default is true]
+
+serialPort.open(function (error) {
+  if ( error ) {
+    console.log('failed to open: '+error);
+  } else {
+    console.log('open');
+    serialPort.on('data', function(data) {
+      console.log('data received: ' + data);
+    });
+    serialPort.write("ls\n", function(err, results) {
+      console.log('err ' + err);
+      console.log('results ' + results);
+    });
+  }
+});
